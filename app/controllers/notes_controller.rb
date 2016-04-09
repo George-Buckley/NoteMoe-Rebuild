19448641daf9a7df133c9811b0f13c53671d9760
George-Buckley <hi@gbuckley.co.uk>
2016-04-09 14:09:57 +0000

    Added Bootstrap Gem, created edit and delete actions, and edit view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,8 +2,13 @@ class NotesController < ApplicationController
     def new
     end
     
+    def edit
+        @note = Note.find(params[:id])
+    end
+    
     def index
         @notes = Note.all
+        @notes = Note.order(created_at: :desc)
     end
     
     def show
@@ -15,6 +20,23 @@ class NotesController < ApplicationController
         
         @note.save
         redirect_to @note
+    end
+    
+    def update
+        @note = Note.find(params[:id])
+
+        if @note.update(note_params)
+            redirect_to @note
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @note = Note.find(params[:id])
+        @note.destroy
+        
+        redirect_to notes_path
     end
     
     private
